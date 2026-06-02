c99f3b6766f69ce7cecb77f4389d66c8b18f831d
HakjisaPublish <sisaem@hakjisa.co.kr>
2026-06-02 15:45:14 +0900

    20-06-01 footer css 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -121,7 +121,7 @@ export default function Footer() {
         >
           <h2
             className="text-[#1E2124] text-lg"
-            style={{ fontFamily: 'Pretendard GOV, Pretendard, sans-serif', fontWeight: 700 }}
+            style={{ fontWeight: 700 }}
           >
             {modal}
           </h2>
@@ -146,95 +146,54 @@ export default function Footer() {
       <div className="w-full border-t border-[#CDD1D5]" />
 
       {/* Surface */}
-      <div className="w-full flex flex-col items-center gap-4 py-8">
-        <div className="flex flex-col gap-6 w-full max-w-[1248px] px-4 md:px-0">
+      <div className="wrap flex flex-col items-center gap-4 py-8">
+        <div className="w-full flex flex-col gap-6">
 
           {/* Row: company-info (left, fill) + external-link (right) */}
-          <div className="flex flex-col md:flex-row md:justify-end md:items-end gap-4 md:gap-6 w-full">
+          <div className="flex flex-col lg:flex-row lg:items-end gap-4">
 
             {/* company-info */}
-            <div className="flex flex-col gap-2 flex-1">
+            <div className="flex flex-col flex-1 gap-4 md:gap-2">
               {/* 대표전화 */}
-              <div className="flex flex-row items-center gap-2">
-                <span
-                  style={{ fontFamily: 'Pretendard GOV, Pretendard, sans-serif', fontWeight: 700, fontSize: 17, lineHeight: '1.5em' }}
-                  className="text-[#1E2124]"
-                >
-                  대표전화
-                </span>
-                <span
-                  style={{ fontFamily: 'Pretendard GOV, Pretendard, sans-serif', fontWeight: 700, fontSize: 17, lineHeight: '1.5em' }}
-                  className="text-[#1E2124]"
-                >
-                  02-330-5170
-                </span>
-                <span
-                  style={{ fontFamily: 'Pretendard GOV, Pretendard, sans-serif', fontWeight: 400, fontSize: 15, lineHeight: '1.5em' }}
-                  className="text-[#1E2124]"
-                >
-                  (평일 09시-18시, 공휴일 제외, 유료)
-                </span>
+              <div className="flex flex-row flex-wrap gap-1 items-center">
+                <dl className="flex flex-row items-center gap-2 font-bold text-[17px]">
+                  <dt>대표전화</dt>
+                  <dd>02-330-5170</dd>
+                </dl>
+                <dl>
+                  <dd>(평일 09시-18시, 공휴일 제외)</dd>
+                </dl>
               </div>
 
               {/* 사업자 정보 */}
-              <div className="flex flex-row gap-4 flex-wrap">
-                <div className="flex flex-row items-center gap-2">
-                  <span
-                    style={{ fontFamily: 'Pretendard GOV, Pretendard, sans-serif', fontWeight: 400, fontSize: 15, lineHeight: '1.5em' }}
-                    className="text-[#1E2124]"
-                  >
-                    통신판매업신고번호
-                  </span>
-                  <span
-                    style={{ fontFamily: 'Pretendard GOV, Pretendard, sans-serif', fontWeight: 400, fontSize: 15, lineHeight: '1.5em' }}
-                    className="text-[#1E2124]"
-                  >
-                    2008-서울마포-0666
-                  </span>
-                </div>
-                <div className="flex flex-row items-center gap-2">
-                  <span
-                    style={{ fontFamily: 'Pretendard GOV, Pretendard, sans-serif', fontWeight: 400, fontSize: 15, lineHeight: '1.5em' }}
-                    className="text-[#1E2124]"
-                  >
-                    사업자등록번호
-                  </span>
-                  <span
-                    style={{ fontFamily: 'Pretendard GOV, Pretendard, sans-serif', fontWeight: 400, fontSize: 15, lineHeight: '1.5em' }}
-                    className="text-[#1E2124]"
-                  >
-                    105-87-03735
-                  </span>
-                </div>
-                <div className="flex flex-row items-center gap-2">
-                  <span
-                    style={{ fontFamily: 'Pretendard GOV, Pretendard, sans-serif', fontWeight: 400, fontSize: 15, lineHeight: '1.5em' }}
-                    className="text-[#1E2124]"
-                  >
-                    이메일
-                  </span>
-                  <span
-                    style={{ fontFamily: 'Pretendard GOV, Pretendard, sans-serif', fontWeight: 400, fontSize: 15, lineHeight: '1.5em' }}
-                    className="text-[#1E2124]"
-                  >
-                    qna@hakjisa.co.kr
-                  </span>
-                </div>
+              <div className="flex flex-col gap-1 md:flex-row md:gap-4">
+                <dl className="flex flex-row items-center gap-2">
+                  <dt>통신판매업신고번호</dt>
+                  <dd>2008-서울마포-0666</dd>
+                </dl>
+                <dl className="flex flex-row items-center gap-2">
+                  <dt>사업자등록번호</dt>
+                  <dd>105-87-03735</dd>
+                </dl>
+                <dl className="flex flex-row items-center gap-2">
+                  <dt>이메일</dt>
+                  <dd>qna@hakjisa.co.kr</dd>
+                </dl>
               </div>
             </div>
 
             {/* external-link: social icons + our-network selectbox */}
-            <div className="flex flex-row items-center gap-4">
+            <div className="flex flex-row items-center gap-4 justify-end">
               {/* social icons */}
               <div className="flex flex-row items-center gap-2">
                 {/* 네이버 블로그 */}
                 <a href="https://blog.naver.com/hjs_newnonmun" target="_blank" rel="noopener noreferrer" aria-label="네이버 블로그" className="flex items-center justify-center w-8 h-8 hover:opacity-80">
                   <img src="/icons/naver_blog.svg" alt="네이버 블로그" style={{ width: 24, height: 24 }} />
-                </a>
+                </a>  
                 {/* 카카오톡 채널 */}
-                <a href="https://pf.kakao.com/_PLxgJj" target="_blank" rel="noopener noreferrer" aria-label="카카오톡 채널" className="flex items-center justify-center w-8 h-8 hover:opacity-80">
+                {/* <a href="https://pf.kakao.com/_PLxgJj" target="_blank" rel="noopener noreferrer" aria-label="카카오톡 채널" className="flex items-center justify-center w-8 h-8 hover:opacity-80">
                   <img src="/icons/kakaotalk_channel.svg" alt="카카오톡 채널" style={{ width: 24, height: 24 }} />
-                </a>
+                </a> */}
               </div>
 
               {/* our-network selectbox */}
@@ -242,7 +201,7 @@ export default function Footer() {
                 <div className="relative w-[200px]">
                   <select
                     className="w-full h-9 px-4 bg-white border border-[#58616A] rounded-[6px] appearance-none cursor-pointer focus:outline-none"
-                    style={{ fontFamily: 'Pretendard GOV, Pretendard, sans-serif', fontWeight: 400, fontSize: 15, lineHeight: '1.5em', color: '#1E2124' }}
+                    style={{ fontSize: 15, color: '#1E2124' }}
                     value=""
                     onChange={e => { if (e.target.value) { window.open(e.target.value, '_blank'); e.target.value = ''; } }}
                   >
@@ -272,34 +231,22 @@ export default function Footer() {
           {/* footer-copyright row */}
           <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-2 w-full">
             {/* terms-link */}
-            <div className="flex flex-row gap-4">
-              <button
-                onClick={() => setModal('이용약관')}
-                className="h-6 px-0.5 hover:underline"
-                style={{ fontFamily: 'Pretendard GOV, Pretendard, sans-serif', fontWeight: 400, fontSize: 15, lineHeight: '1.5em', color: '#1E2124' }}
-              >
+            <div className="terms-link flex flex-row gap-4">
+              <a onClick={() => setModal('이용약관')}>
                 이용약관
-              </button>
-              <button
-                onClick={() => setModal('개인정보처리방침')}
-                className="h-6 px-0.5 hover:underline"
-                style={{ fontFamily: 'Pretendard GOV, Pretendard, sans-serif', fontWeight: 400, fontSize: 15, lineHeight: '1.5em', color: '#1E2124' }}
-              >
+              </a>
+              <a onClick={() => setModal('개인정보처리방침')}>
                 개인정보처리방침
-              </button>
-              <button
-                onClick={() => setModal('이용문의')}
-                className="h-6 px-0.5 hover:underline"
-                style={{ fontFamily: 'Pretendard GOV, Pretendard, sans-serif', fontWeight: 400, fontSize: 15, lineHeight: '1.5em', color: '#1E2124' }}
-              >
+              </a>
+              <a onClick={() => setModal('이용문의')}>
                 이용문의
-              </button>
+              </a>
             </div>
 
             {/* copyright */}
-            <div className="flex items-center h-6">
+            <div className="copyright flex items-center">
               <span
-                style={{ fontFamily: 'Pretendard GOV, Pretendard, sans-serif', fontWeight: 400, fontSize: 13, lineHeight: '1.5em', color: '#464C53' }}
+                style={{ fontSize: 13, color: '#464C53' }}
               >
                 © HAKJISA, INC. All rights reserved.
               </span>
